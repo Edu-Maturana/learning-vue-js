a56fc393f05902f0d3640221ca6746b62e0bd841
Edu Maturana <edu@pop-os.localdomain>
2021-11-09 00:33:23 -0300

    Binding condicional

--- src/components/Peliculas.vue
+++ src/components/Peliculas.vue
@@ -15,7 +15,11 @@
         </div>
 
         <h2>{{ pelicula.titulo }}</h2>
-        <span class="date"> Hace 5 minutos </span>
+        <span class="date" :class="{
+            green: pelicula.year >= 2018,
+            red: pelicula.year < 2018}"> 
+            {{pelicula.year}}
+        </span>
         <a href="#">Leer más</a>
 
         <div class="clearfix"></div>
@@ -35,24 +39,28 @@ export default {
         {
           id: 1,
           titulo: "Star wars",
+          year: 2017,
           imagen:
             "https://i1.wp.com/hipertextual.com/wp-content/uploads/2020/05/hipertextual-star-wars-celebra-may-the-4th-be-with-you-con-extraordinario-video-que-rinde-homenaje-toda-saga-2020581511.jpg?fit=1920%2C1080&ssl=1",
         },
         {
           id: 2,
           titulo: "Black widow",
+          year: 2019,
           imagen:
             "https://ca-times.brightspotcdn.com/dims4/default/a4c7991/2147483647/strip/true/crop/2506x1670+747+0/resize/2000x1333!/quality/90/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2Fbf%2F52%2F126190d94252ac33cdfb09597113%2Fla-ca-black-widow-movie-033.JPG",
         },
         {
           id: 3,
           titulo: "Spiderman",
+          year: 2018,
           imagen:
             "https://as01.epimg.net/meristation/imagenes/2021/11/08/noticias/1636368945_398535_1636368978_noticia_normal.jpg",
         },
         {
           id: 4,
           titulo: "Leo Messi",
+          year: 2019,
           imagen:
             "https://www.futbolred.com/files/article_main/files/crop/uploads/2021/10/29/617c5c491feff.r_1635540113147.0-0-3480-1744.jpeg",
         },
@@ -61,5 +69,3 @@ export default {
   },
 };
 </script>
-
-<style></style>
